cada28dc3fadf32afcc35649737b37a83d8cd27f
Angeleen MCDONNELL Wilson <angeleeen@dignyfy.com>
2019-11-22 06:18:09 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -4,20 +4,10 @@ board[0]
 board[1]
 board[2]
 board[0]="X"
-board #=> ["X"]," "," "]
 board[1]="O"
-board #=> [" "],"O"," "]
 board[2]="X"
-board #=> [" "]," ","X"]
 board[0]="O"
-board #=> [" "],"O"," "]
-board[0]="X"
-board #=> [" "]," ","X"]
 board[1]="O"
-board #=> [" "]," ","O"]
 board[1]="X"
-board #=> ["X"]," "," "]
 board[2]="O"
-board #=> ["O"]," "," "]
 board[2]="X"
-board #=>[" ","X"," "]
