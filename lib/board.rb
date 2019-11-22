70dd02cf86e6232a41d427780fe6c19601451afb
Angeleen MCDONNELL Wilson <angeleeen@dignyfy.com>
2019-11-22 06:20:10 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,13 +1,14 @@
 # Define the variable board below.
-board = ["0","1","2"]
+board = ["0","1","2","3","4","5","6","7","8"]
 board[0]
 board[1]
 board[2]
 board[0]="X"
 board[1]="O"
 board[2]="X"
-board[0]="O"
-board[1]="O"
-board[1]="X"
-board[2]="O"
-board[2]="X"
+board[3]="O"
+board[4]="X"
+board[5]="X"
+board[6]="O"
+board[7]="X"
+board[8]="0"
